2341719563cb393945b1549c522df08c68f18f46
zjoasan <joakim.s@webbkontakt.net>
2016-04-01 16:29:29 +0200

    Update install_web.sh

--- install_web.sh
+++ install_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
 sudo apt-get --show-progress -y install dialog arora fbi libexif12  2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing FBI imageviewer and Arora" --gauge "\nPlease wait...\n" 11 70
-wget -O install_vnc.zip https://github.com/zjoasan/directvnc_install/blob/master/install_vnc.zip?raw=true
+wget -O install_vnc.zip https://github.com/zjoasan/directvnc_install/blob/master/install_web.zip?raw=true
 unzip -q -o install_web.zip 
 chmod +x /home/osmc/web-browser/webbis.sh 2>&1
 chmod +x /home/osmc/web-browser/webstart2.sh 2>&1

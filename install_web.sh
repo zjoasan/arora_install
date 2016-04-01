66be404ebbfebcbe8ca0973fcab3c91854a42a28
zjoasan <joakim.s@webbkontakt.net>
2016-04-01 15:30:13 +0200

    Added files via upload

---
+++ install_web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
+sudo apt-get --show-progress -y install dialog arora fbi libexif12  2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing FBI imageviewer and Arora" --gauge "\nPlease wait...\n" 11 70
+wget -q http://osmc.webbkontakt.net/install_web.zip 
+unzip -q -o install_web.zip 
+chmod +x /home/osmc/web-browser/webbis.sh 2>&1
+chmod +x /home/osmc/web-browser/webstart2.sh 2>&1
+chmod +x /home/osmc/web-browser/web_watchdog.sh 2>&1
+cp /home/osmc/.kodi/addons/plugin.program.www-launcher/fanart.jpg /home/osmc/web-browser/fanart.jpg
+rm -f install_web.zip 2>&1
+dialog --title "Installation finnished!" --msgbox "\nThank you for using my installer\n"  11 70
+exit
+
+
+
